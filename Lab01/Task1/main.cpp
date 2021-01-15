4659ba4d1f79b3d4ed54dccd5400b06b664ae38f
karupoiss00 <glizeron1108@gmail.com>
2021-01-16 00:08:04 +0300

    Выпилил подсчет 15 и 20 элементов

--- Lab01/Task1/main.cpp
+++ Lab01/Task1/main.cpp
@@ -24,13 +24,8 @@ double GetGeneratingPermutationsTime(array<size_t, ARRAY_SIZE> arr)
 int main()
 {
 	array<size_t, 10> v1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	array<size_t, 15> v2 = { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	array<size_t, 20> v3 = { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
 
 	cout << "10: " << GetGeneratingPermutationsTime(v1) << "s" << endl;
-	cout << "15: " << GetGeneratingPermutationsTime(v2) << "s" << endl;
-	cout << "20: " << GetGeneratingPermutationsTime(v3) << "s" << endl;
 
 	return 0;
 }
