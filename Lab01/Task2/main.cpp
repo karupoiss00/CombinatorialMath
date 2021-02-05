8837cacbf8a0e3178a2b213d3d3ffe081c1cf7ba
karupoiss00 <glizeron1108@gmail.com>
2021-02-06 00:51:09 +0300

    Второе задание допил

--- Lab01/Task2/main.cpp
+++ Lab01/Task2/main.cpp
@@ -5,88 +5,108 @@
 #include <boost/timer.hpp>
 
 using namespace std;
-
-struct Element {
-
-	Element() {
-		this->value = 0;
-		this->isRightDirection = false;
-	}
-
-	Element(size_t value, bool rightDir) {
-		this->value = value;
-		this->isRightDirection = rightDir;
-	}
-
-	bool operator<<(Element el) const {
-		cout << this->value;
-	}
-	bool operator<(Element el) const {
-		return this->value < el.value;
-	}
-	bool operator>(Element el) const {
-		return this->value > el.value;
-	}
-
-	size_t value;
-	bool isRightDirection;
-};
-
+/*
 template<size_t ARRAY_SIZE>
-array<size_t, ARRAY_SIZE> ConvertToArrayOfValue(array<Element, ARRAY_SIZE> arr) {
-	array<size_t, ARRAY_SIZE> newArr;
-	for (size_t i = 0; i < ARRAY_SIZE; i++) {
-		newArr[i] = arr[i].value;
-	}
-	return newArr;
-}
-
-template<size_t ARRAY_SIZE>
-double GetGeneratingPermutationsTime(array<Element, ARRAY_SIZE> arr)
+double GetGeneratingPermutationsTime(array<size_t, ARRAY_SIZE> arr)
 {
-	size_t m = -1;
-	size_t mAsValueIndex;
-	array<size_t, ARRAY_SIZE> valuesArray = ConvertToArrayOfValue(arr);
+	int m = -1;
+	int maxElement = 0;
+	array<int, ARRAY_SIZE> directions;
+	for (size_t i = 0; i < directions.size(); ++i)
+	{
+		directions[i] = -1;
+	}
+	arr[0] = ARRAY_SIZE + 1;
+	directions[1] = 0;
+	array<size_t, ARRAY_SIZE> currentPermutation = arr;
+	array<size_t, ARRAY_SIZE> reversedCurrentPermutation = arr;
 	boost::timer t;
 	t.restart();
-	sort(arr.begin(), arr.end());
-	
-	while (m != 0)
+	m = ARRAY_SIZE - 1;
+	maxElement = ARRAY_SIZE - 1;
+	while (m != 1)
 	{
-		copy(valuesArray.begin(), valuesArray.end(), ostream_iterator<size_t>(cout, " "));
-		cout << endl;
-		m = ARRAY_SIZE - 1;
-		while (m > 0 && arr[m] < arr[m - 1])
+		copy(currentPermutation.begin(), currentPermutation.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		cout << endl << reversedCurrentPermutation[9] << endl;
+		m = maxElement;
+		cout  << "curr perm " << currentPermutation[reversedCurrentPermutation[m] + directions[m]] << "< "<< m << endl;
+		while (currentPermutation[reversedCurrentPermutation[m] + directions[m]] > m)
 		{
+			directions[m] = -directions[m];
 			m--;
 		}
-		mAsValueIndex = distance(valuesArray.begin(), find(valuesArray.begin(), valuesArray.end(), m));
-		swap(arr[m], arr[mAsValueIndex]);
-		valuesArray = ConvertToArrayOfValue(arr);
-		for (size_t i = 0; i < arr.size(); i++)
+		swap(currentPermutation[m], currentPermutation[m + directions[m]]);
+		swap(reversedCurrentPermutation[currentPermutation[reversedCurrentPermutation[m]]], reversedCurrentPermutation[m]);
+		for (size_t i = 0; i < directions.size(); i++)
 		{
-			if (arr[i].value > m)
+			if (arr[i] > m)
 			{
-				arr[i].isRightDirection = !arr[i].isRightDirection;
+				directions[i] = -directions[i];
 			}
 		}
 	}
 
 	double duration = t.elapsed();
-	return duration;	
+	return duration;
+}*/
+
+template<size_t ARRAY_SIZE>
+double GetGeneratingPermutationsTime(array<size_t, ARRAY_SIZE> arr)
+{
+	array<size_t, ARRAY_SIZE> reversedArr = arr;
+	array<int, ARRAY_SIZE> directions;
+	int m = ARRAY_SIZE + 1;
+	for (size_t i = 0; i < directions.size(); ++i)
+	{
+		directions[i] = -1;
+	}
+	directions[1] = 0;
+ 	arr[0] = m;
+	arr[ARRAY_SIZE - 1] = m;	
+	boost::timer t;
+	t.restart();
+	while (m != 1)
+	{
+		//copy(arr.begin(), arr.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		//cout << endl;
+		m = ARRAY_SIZE - 2;
+		while (arr[reversedArr[m] + directions[m]] > m)
+		{
+			directions[m] = -directions[m];
+			m--;
+		}
+		swap(arr[reversedArr[m]], arr[reversedArr[m] + directions[m]]);
+		swap(reversedArr[arr[reversedArr[m]]], reversedArr[m]);
+	}
+
+	double duration = t.elapsed();
+	return duration;
 }
+
 
 int main()
 {
-	array<Element, 10> arr;
-
-	for (size_t i = 0; i < arr.size(); ++i)
+	array<size_t, 12> arr10;
+	for (size_t i = 1; i < arr10.size(); ++i)
 	{
-		arr[i].value = i;
-		arr[i].isRightDirection = false;
+		arr10[i] = i;
 	}
 
-	cout << GetGeneratingPermutationsTime(arr) << "s" << endl;
+	array<size_t, 15> arr15;
+	for (size_t i = 1; i < arr15.size(); ++i)
+	{
+		arr15[i] = i;
+	}
+
+	array<size_t, 22> arr20;
+	for (size_t i = 1; i < arr20.size(); ++i)
+	{
+		arr20[i] = i;
+	}
+
+	//cout << GetGeneratingPermutationsTime<arr10.size()>(arr10) << "s" << endl;
+	cout << GetGeneratingPermutationsTime<arr15.size()>(arr15) << "s" << endl;
+	//cout << GetGeneratingPermutationsTime<arr20.size()>(arr20) << "s" << endl;
 
 	return 0;
 }
